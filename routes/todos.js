f855ed5c7de1b81449b09b6ba958d872557f02b2
dhyaneshsiddhartha15 <siddharthadhyanesh7@gmail.com>
2024-02-20 12:31:11 +0530

    Initial commit

---
+++ routes/todos.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { createTodo } = require("../controllers/createTodo");
+const { getTodo } = require("../controllers/getTodo");
+const { updateTodo } = require("../controllers/updateTodo");
+const { deleteTodo } = require("../controllers/deleteTodo");
+
+router.post("/createTodo", createTodo);
+router.get("/getTodos", getTodo);
+router.put("/updateTodo/:id", updateTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
+
+module.exports = router;
